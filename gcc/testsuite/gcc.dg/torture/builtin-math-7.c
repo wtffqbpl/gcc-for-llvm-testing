c925a0cfc8bf1985c77dcd7bd6eee05e797b96f8
Paolo Savini <paolo.savini@embecosm.com>
2019-11-01 13:45:10 +0000

    Annotate tests that check math operations are folded correctly by GCC as requiring gcc frontend.
    gcc/testsuite/ChangeLog.Embecosm:
    
            * gcc.dg/torture/builtins-math-6.c: Annotate test as requiring
              GCC frontend.
            * gcc.dg/torture/builtins-math-7.c: Likewise.
            * gcc.dg/torture/builtins-isinf_sign-1.c: Likewise.

--- gcc/testsuite/gcc.dg/torture/builtin-math-7.c
+++ gcc/testsuite/gcc.dg/torture/builtin-math-7.c
@@ -10,6 +10,7 @@
 /* { dg-add-options ieee } */
 /* { dg-require-effective-target large_double } */
 /* { dg-require-effective-target builtin_isinf } */
+/* { dg-require-effective-target gcc_frontend } */
 
 extern void link_error(int);
 
